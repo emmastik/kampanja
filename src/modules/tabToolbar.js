2e3c4386b28e6ffb68777818ea11450767e73677
Emma <eastikainen@gmail.com>
2023-05-19 23:03:57 +0300

    Fixes DOM warnings

--- src/modules/tabToolbar.js
+++ src/modules/tabToolbar.js
@@ -48,7 +48,7 @@ function TabToolbar() {
 
   return (
     /* Includes all text components and a video component in different tabs */
-    <Box sx={{ width: '100%' }}>
+    <Box sx={{ width: '100' }}>
       <Tabs
         value={value}
         onChange={handleChange}
@@ -66,41 +66,36 @@ function TabToolbar() {
         <Tab label="Linkkejä" {...a11yProps(2)} />
         <Tab label="Kampanjavideo" {...a11yProps(3)} />
       </Tabs>
-      <TabPanel value={value} index={0}>
-        <Typography className="font" sx={{ mt: 3, mb: 3}}>
-          Löytöeläimien määrä on ongelma kaikkialla maailmassa, myös Suomessa. Meillä Suomessakin
-          on paljon eläimiä, jotka on löydetty yksin luonnosta kulkemasta tai muuten otettu hoitoon
-          ja tarvitsevat näin kotia sekä hoivaa.<br/><br/>
+      <TabPanel value={value} index={0} sx={{ mt: 3, mb: 3}}>
+        Löytöeläimien määrä on ongelma kaikkialla maailmassa, myös Suomessa. Meillä Suomessakin
+        on paljon eläimiä, jotka on löydetty yksin luonnosta kulkemasta tai muuten otettu hoitoon
+        ja tarvitsevat näin kotia sekä hoivaa.<br/><br/>
 
-          "Adoptoi, älä osta" -kampanjavideon löydät viimeiseltä välilehdeltä - käy katsomassa!<br/>
-          Alta löydät myös chatin, johon voit laittaa kommenttia tai kysymyksiä sekä tykätä muiden viesteistä.<br/>
-          Vierestä (pienellä näytöllä alhaalta) löydät myös videossa vierailevan Harmin sekä ystäväni rescue -kissan Cleon.
-        </Typography>
+        "Adoptoi, älä osta" -kampanjavideon löydät viimeiseltä välilehdeltä - käy katsomassa!<br/>
+        Alta löydät myös chatin, johon voit laittaa kommenttia tai kysymyksiä sekä tykätä muiden viesteistä.<br/>
+        Vierestä (pienellä näytöllä alhaalta) löydät myös videossa vierailevan Harmin sekä ystäväni rescue -kissan Cleon.
       </TabPanel>
-      <TabPanel value={value} index={1}>
-        <Typography className="font" sx={{ mt: 3, mb: 3}}>
-          Miten mahdollista?<br/>
-          Suomesta löytyy eri paikkakunnilta adoptoimiseen mahdollistavia keskuksia, kuten 
-          löytö- ja sijaiseläinkoteja, joihin voi hakemusta lemmikin adoptoimisesta lähettää.
-          Kannattaa Googlata yleistä tietoa adoptoimisesta sekä itse adoptioprosessista.<br/><br/>
-          Missä mahdollista?<br/>
-          Mahdollista missä vain. Löytöeläinkodeilla on omat sijaintinsa, mutta tämä ei kuitenkaan estä 
-          tekemästä matkaa adoptoidakseen lemmikin.<br/><br/>
-          Miksi adoptoida?<br/>
-          Adoptoiminen on eettistä sekä tukee kestävää kehitystä. 
-          Lisäksi monet lemmikit tarvitsevat apua, ja tätä apua voi heille tarjota antamalla kodin sekä 
-          huolenpitoa ja rakkautta.<br/><br/>
-          Mitä lemmikin adoptoiminen maksaa?<br/>
-          Lemmikkiä adoptoidessa on joitain kustannuksia Suomessa, ja adoptio saattaa maksaa 
-          muutamia satoja euroa. Tämä maksu auttaa löytöeläinkoteja rahoittamaan toimintaansa sekä 
-          varmistamaan lemmikin terveyden ennen adoptiota.<br/><br/>
-          Löytö- ja sijaiseläinkotitoiminnan tukeminen<br/>
-          Toimintaa mahdollista tukea rahallisesti lahjoituksen muodossa, ostamalla toimintaa 
-          tukevia tuotteita sekä osallistumalla tarvikekeräyksiin kaupoissa.
-        </Typography>
+      <TabPanel value={value} index={1} className="font" sx={{ mt: 3, mb: 3}}>
+        Miten mahdollista?<br/>
+        Suomesta löytyy eri paikkakunnilta adoptoimiseen mahdollistavia keskuksia, kuten 
+        löytö- ja sijaiseläinkoteja, joihin voi hakemusta lemmikin adoptoimisesta lähettää.
+        Kannattaa Googlata yleistä tietoa adoptoimisesta sekä itse adoptioprosessista.<br/><br/>
+        Missä mahdollista?<br/>
+        Mahdollista missä vain. Löytöeläinkodeilla on omat sijaintinsa, mutta tämä ei kuitenkaan estä 
+        tekemästä matkaa adoptoidakseen lemmikin.<br/><br/>
+        Miksi adoptoida?<br/>
+        Adoptoiminen on eettistä sekä tukee kestävää kehitystä. 
+        Lisäksi monet lemmikit tarvitsevat apua, ja tätä apua voi heille tarjota antamalla kodin sekä 
+        huolenpitoa ja rakkautta.<br/><br/>
+        Mitä lemmikin adoptoiminen maksaa?<br/>
+        Lemmikkiä adoptoidessa on joitain kustannuksia Suomessa, ja adoptio saattaa maksaa 
+        muutamia satoja euroa. Tämä maksu auttaa löytöeläinkoteja rahoittamaan toimintaansa sekä 
+        varmistamaan lemmikin terveyden ennen adoptiota.<br/><br/>
+        Löytö- ja sijaiseläinkotitoiminnan tukeminen<br/>
+        Toimintaa mahdollista tukea rahallisesti lahjoituksen muodossa, ostamalla toimintaa 
+        tukevia tuotteita sekä osallistumalla tarvikekeräyksiin kaupoissa.
       </TabPanel>
-      <TabPanel value={value} index={2}>
-      <Typography className="font" sx={{ mt: 3, mb: 3}}>
+      <TabPanel value={value} index={2} className="font" sx={{ mt: 3, mb: 3}}>
         Yleistä tietoa löytöeläimistä sekä adoptoimisesta:<br/>
         Suomen eläinsuojelu: 
           <a href="https://sey.fi/" target="_blank" rel="noopener noreferrer">sey.fi</a><br/>
@@ -117,7 +112,6 @@ function TabToolbar() {
         
         Löytöeläinkoteja, niiden informaatiota sekä itse löytöeläimet löytyvät heidän omilta sivuiltaan.
         Löytöeläinkoteja voi esimerkiksi Googlata kaupungin tai maakunnan avulla.
-      </Typography>
       </TabPanel>
       <TabPanel value={value} index={3}>
         <iframe 
@@ -125,9 +119,8 @@ function TabToolbar() {
           height="315" 
           src="https://www.youtube.com/embed/Ll_gZ35x0k0" 
           title="YouTube video player" 
-          frameborder="0" 
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-          allowfullscreen>
+          allowFullScreen>
         </iframe>
       </TabPanel>
     </Box>
