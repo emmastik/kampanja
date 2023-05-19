208428a91588b7ef0cf62a2a27ee9e863299cf48
Emma <eastikainen@gmail.com>
2023-05-19 16:53:17 +0300

    Adds cat images on the page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,59 @@
 import './App.css';
-import { Card, CardContent } from '@mui/material';
+import { Card, CardContent, Grid, ImageList, ImageListItem, ImageListItemBar } from '@mui/material';
 import * as React from 'react';
 import TabToolbar from './modules/tabToolbar.js';
 import Chat from './modules/chat.js';
 
 
 function App() {
+  const itemData = [
+    {
+      img: "./images/cleo.JPG",
+      title: "Cleo",
+    },
+    {
+      img: "./images/harmi.JPG",
+      title: "Harmi"
+    },
+  ];
 
   return (
     <div className="App">
       <header className="header">
         <h2 style={{marginLeft: "3vw"}}>Adoptoi lemmikki, älä osta<br></br>-kampanjasivu</h2>
       </header>
-      <Card sx={{ width: "45%", ml: 3 }}> 
-        <CardContent>
-          <TabToolbar />
-        </CardContent>
-      </Card>
-      <Card sx={{mt: 6, ml: 3, width: "45%"}}>
-        <CardContent>
-          <Chat />
-        </CardContent>
-      </Card>
-      <div>
-        {/* <img src="./images/cleo.jpg" alt="cleo" width="400" height="550"/> */}
-      </div>
+      <Grid container direction="row" justifyContent="flex-start"> 
+        <Grid item xs={10} lg={6}>
+          <Card sx={{ minWidth: "45%", maxWidth: "90%", ml: 3 }}> 
+            <CardContent>
+              <TabToolbar />
+            </CardContent>
+          </Card>
+          <Card sx={{mt: 6, ml: 3, minWidth: "45%", maxWidth: "90%"}}>
+            <CardContent>
+              <Chat />
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={5} lg={5}>
+          <ImageList sx={{width: "100%", height: "100%" }} cols={2}>
+            {itemData.map((item) => (
+              <ImageListItem key={item.img}>
+                <img
+                  src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                  alt={item.title}
+                  loading="lazy"
+                />
+                <ImageListItemBar 
+                  sx={{background: "transparent"}}
+                  title={item.title}
+                  position='top'
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </Grid>
+      </Grid>
     </div> 
   );
 }
--- src/modules/chat.js
+++ src/modules/chat.js
@@ -67,7 +67,6 @@ function Chat() {
             >
                 Lähetä
             </button>
-            {JSON.stringify(messages)}
             <div>
                 <MessageList 
                     messageItems={messages}
